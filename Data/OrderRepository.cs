674be600df064bc5114c2f4eae8801271135ec79
iben90 <ikhl.beni@gmail.com>
2025-12-09 13:35:49 +0100

    refactor(program): await async methods

--- Data/OrderRepository.cs
+++ Data/OrderRepository.cs
@@ -4,16 +4,15 @@ using LegacyOrderService.Models;
 
 namespace LegacyOrderService.Data
 {
-    public class OrderRepository :IOrderRepository
+    public class OrderRepository : IOrderRepository
     {
         private string _connectionString = $"Data Source={Path.Combine(AppContext.BaseDirectory, "orders.db")}";
 
-
-        public void Save(Order order)
+        public async Task SaveAsync(Order order)
         {
             using (var connection = new SqliteConnection(_connectionString))
             {
-                connection.Open();
+                await connection.OpenAsync();
                 using (var command = connection.CreateCommand())
                 {
                     command.CommandText = @"INSERT INTO Orders (CustomerName, ProductName, Quantity, Price) VALUES (@customer, @product, @quantity, @price)";
@@ -21,24 +20,25 @@ namespace LegacyOrderService.Data
                     command.Parameters.AddWithValue("@product", order.ProductName);
                     command.Parameters.AddWithValue("@quantity", order.Quantity);
                     command.Parameters.AddWithValue("@price", order.Price);
-                    command.ExecuteNonQuery();
+                    await command.ExecuteNonQueryAsync();
                 }
             }
         }
 
-        public void SeedBadData()
+        public async Task SeedBadDataAsync()
         {
             using (var connection = new SqliteConnection(_connectionString))
             {
-                connection.Open();
+                await connection.OpenAsync();
                 using (var cmd = connection.CreateCommand())
                 {
                     cmd.CommandText = "INSERT INTO Orders (CustomerName, ProductName, Quantity, Price) VALUES ('John', 'Widget', 9999, 9.99)";
-                    cmd.ExecuteNonQuery();
+                    await cmd.ExecuteNonQueryAsync();
                 }
-
             }
         }
+
+
     }
 }
 
